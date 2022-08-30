e8a072e91bb9e8cd78f7d1d35582655b531ef7ce
akamdsm <85087213+akamdsm@users.noreply.github.com>
2022-08-29 22:47:20 -0300

    Update portscan.py

--- portscan.py
+++ portscan.py
@@ -1,6 +1,6 @@
 import socket, sys
 
-for port in range(1,100):
+for port in range(1,65535):
 	mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	if(mysocket.connect_ex((sys.argv[1], port))) == 0:
 		print("The port {} is open".format(port))
